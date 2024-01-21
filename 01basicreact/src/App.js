8d5e176bd733a156918e3354d020c112182f0626
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-01-21 08:52:09 +0530

    cleaning code

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+// function App() {
+//   return (
+//     <h1>Chai aur React</h1>
+//   );
+// }
+
+// export default App;
+function App(){
+  return(
+    <><h1>Chai aur React || Suraj Kumar Verma </h1>
+    <div>Hey I'm Starting React Project from today with hitesh sir it is good experience till now 
+      the way of teaching is owsome let's continue the journey</div></>
+  );
+}
+export default App;                    
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1>Chay aur React | Ram Ram bhai sare ne </h1>      
+    </>
+  )
+}
+
+export default App;
