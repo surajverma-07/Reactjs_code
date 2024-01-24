bc43b4a58790375af672b99cec82155e805164a1
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-01-24 19:53:33 +0530

    Learning tailwind and Props

---
+++ 07TailwindProps/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username : "suraj",
+    age : 20,
+    id : 7,
+    collage : "BIT DURG"
+
+  }
+  
+  let myArr = [7,10,18,8];
+  return (
+    <>
+     {/* p-4 padding */}
+       <h1 className='bg-green-500 text-black p-4 mb-4 rounded-xl' >Tailwind with Suraj7</h1>
+       {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/14303820/pexels-photo-14303820.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+       </figure> */}
+      {/* <Card channel="chaiaurcode" myArr=[1,2,3]/> */}
+      {/* <Card channel="chaiaurcode" myArr={nam :"chai"}/> */}
+      {/* <Card channel="chaiaurcode" Obj = {myObj}/> */}
+      {/* <Card obj1={myArr} /> */}
+      <Card username="Suraj Kumar Verma" btntxt="Explore more" />
+      <Card username="Dharana" btntxt="Commited message"/>
+      <Card username="us" />
+
+    </>
+  )
+}
+
+export default App
---
+++ 07TailwindProps/src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+ 
+// function Card(props){
+function Card({username , btntxt="visit page"}){
+    // console.log("props ", props)
+    // console.log(username)
+    return(
+        <div className="w-[300px] rounded-md border ml-20">
+        <img
+          src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Laptop"
+          className="h-[200px] w-full rounded-md object-cover"
+        />
+        <div className="p-4">
+          <h1 className="text-lg font-semibold">About {username}</h1>
+          <p className="mt-3 text-sm text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button
+            type="button"
+            className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            {/* {props.btntxt || "visit page"}  */}
+            {/* or function if btntxt not passed */}
+            {btntxt}
+          </button>
+        </div>
+      </div>
+    )
+}
+export default Card
