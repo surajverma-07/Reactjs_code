f5002263b7d2b0629494fa620eacef1a80b0bc4b
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-02-12 17:17:03 +0530

    Learning Routing through new Portfolio website with the help of react

---
+++ 11Portfolio_Routing/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-xl bg-red-400'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
