af9a5ba8b541a82b67a405ee780ecb70e7636d0d
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-13 20:57:26 +0530

    some changes in card

--- 13ContextApiProject/src/components/Card.jsx
+++ 13ContextApiProject/src/components/Card.jsx
@@ -3,12 +3,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/16300464/pexels-photo-16300464/free-photo-of-biker-in-rural-scenery.jpeg?auto=compress&cs=tinysrgb&w=600" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        suraj Kumar Verma || Learning React JS || A Passionate Web Developer 
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
