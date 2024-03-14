28286b0512e6f97a9172e21696dc638c8cbe2714
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-14 18:45:27 +0530

    Todo task Project initiated and context are written

---
+++ 14todoContext/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import {createContext,useContext} from "react"
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id:1,
+            todo:"message",
+            done:false,
+        },
+    ],
+    update : (id , todo ) => {},
+    add : (todo) => {},
+    delete : (id) => {},
+    toggle:(id) => {}
+
+})
+
+export const useTodo = () =>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider 
