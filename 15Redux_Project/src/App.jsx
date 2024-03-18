2a19ff6c50e2bb0f6cdafefa3038c4a88d448378
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-18 19:39:50 +0530

    Redux toolkit project initiated

--- 15Redux_Project/src/App.jsx
+++ 15Redux_Project/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       
+      <h1 className='bg-orange-400 text-blue-950 text-center'>Jay Shree Ram || Redux Toolkit</h1>
     </>
   )
 }
