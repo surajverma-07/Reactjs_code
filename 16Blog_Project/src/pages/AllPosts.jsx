f0a57d3aee3f161a7ff6f8d8ad4e402bdebebe50
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-04-09 09:19:44 +0530

    all project section improved

--- 16Blog_Project/src/pages/AllPosts.jsx
+++ 16Blog_Project/src/pages/AllPosts.jsx
@@ -16,9 +16,9 @@ function AllPosts() {
   return (
     <div className='w-full py-8'>
         <Container>
-            <div className='flex flex-wrap'>
+            <div className='flex flex-wrap justify-evenly'>
                 {posts.map((post)=>(
-                    <div key={post.$id} className='p-2 w-1/4'>
+                    <div key={post.$id} className='p-2'>
                         <PostCard {...post} />
                     </div>
                 ))}
