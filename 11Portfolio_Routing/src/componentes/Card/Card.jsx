0f003e614090cdd3aa756fe97ca80f9f146060a6
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-05-10 09:51:40 +0530

    new project added and button position fixed

--- 11Portfolio_Routing/src/componentes/Card/Card.jsx
+++ 11Portfolio_Routing/src/componentes/Card/Card.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 function Card({image,Heading="website" , weblink="#",text="ye h hamari website "}){
 
     return(
-        <div className="md:w-[350px] w-full m-5  rounded-lg border shadow-md  hover:bg-[#FC6736] hover:scale-105  group dark:bg-[#171717]  ">
+        <div className="md:w-[350px] w-full h-[33rem] m-5 relative rounded-lg border shadow-md  hover:bg-[#FC6736] hover:scale-105  group dark:bg-[#171717]  ">
         <img
           src={image}
           alt="Laptop"
           className="h-[250px] w-full  object-fit rounded-lg"
         />
         <div className="py-3 text-center w-full ">
-          <h1 className="text-3xl text-[#0C2D57] font-semibold dark:text-[#DA0037]">{Heading}</h1>
+          <h1 className="text-2xl text-[#0C2D57] font-semibold dark:text-[#DA0037]">{Heading}</h1>
           <p className="mt-3 text-md text-gray-800 px-4 dark:text-[#EDEDED] group-hover:text-[#EFECEC] ">
                  {text}
           </p>
           <button
             type="button"
-            className="mt-4 rounded-md bg-[#ec5b53] dark:bg-[#DA0037] px-6 py-1  text-xl font-semibold group-hover:bg-[#0C2D57] dark:group-hover:bg-[#444444] dark:group-hover:text-[#DA0037] text-[#EFECEC]  shadow-sm focus-visible:outline  focus-visible:outline-2 focus-visible:outline-offset-2 "
+            className="mt-4 rounded-md bg-[#ec5b53] dark:bg-[#DA0037] px-6 py-1  text-xl font-semibold group-hover:bg-[#0C2D57] dark:group-hover:bg-[#444444] dark:group-hover:text-[#DA0037] text-[#EFECEC]  shadow-sm focus-visible:outline  focus-visible:outline-2 focus-visible:outline-offset-2 absolute bottom-3 left-20"
             
           >
             <a href={weblink} target='_blank'>Go To Website</a>
--- 11Portfolio_Routing/src/componentes/Project/Project.jsx
+++ 11Portfolio_Routing/src/componentes/Project/Project.jsx
@@ -6,19 +6,20 @@ import project3 from '../../assets/project3.png';
 import gym from '../../assets/project1.png';
 import food from '../../assets/project2.png';
 import project4 from '../../assets/password.png';
+import spm from '../../assets/spm.png'
 
 function Project() {
   return (
     <div className='bg-[#EFECEC] dark:bg-[#444444]'>
       <div className='flex flex-wrap w-full m-0 p-0  items-center justify-center text-center mt-4'>
 
+        <Card className='w-full' image={spm} Heading="Student Project Manager" weblink='https://studentprojectmanager.netlify.app/' text='A student project management website enables authenticated students to submit project details, including screenshots, synopsis, and presentations. It facilitates project tracking for students and assessment for teachers, allowing students to add, update, and delete their projects while viewing all submissions.' />
         <Card className='w-full' image={project1} Heading="Manage To Do" weblink='https://settodos.netlify.app/' text='SetTodos is a user-friendly todo management website built with the power of ReactJS, Tailwind CSS, HTML, and JavaScript. This intuitive platform empowers users to effortlessly organize their tasks by providing essential features such as adding, updating, deleting, and toggling todos.' />
 
         <Card image={project2} Heading='Personal Portfolio' weblink='https://cuwebdevsuraj.netlify.app/' text='The Portfolio Showcase, a dynamic site, is a collaborative effort between me and Aaditya. Crafted with precision, it employs HTML, CSS, and JavaScript. Its visually captivating design offers seamless navigation and showcases our collective skills and creativity.' />
         <Card image={project3} Heading='E-Waste Facility Locator' weblink='https://surajverma-07.github.io/E-Waste-Facility-Locator/' text='Team Coders United developed an E-Waste facility locator connecting people to Indian authorities for dismantling E-Waste. This solution addresses the harmful impact of electronic waste, facilitating responsible disposal and contributing to environmental sustainability.' />
         <Card image={project4} Heading='Password Generator' weblink='https://randompasswordsgenrator.netlify.app/' text='A versatile Random Password Generator website, powered by React JS, JavaScript, and Tailwind CSS. Users can customize passwords by length, including/excluding numbers and special characters. This intuitive tool offers seamless control for generating secure passwords tailored to individual preferences.' />
         <Card image={food} Heading='Food Delivery Website' weblink="https://surajverma-07.github.io/fooddeliveryportfolio/" text='A food delivery website, crafted solely with HTML and CSS, offers a simple UI. Users navigate effortlessly, exploring menus and placing orders. Though lacking a backend, it provides a seamless experience for browsing and selecting meals.' />
-        <Card image={gym} Heading='Gym Portfolio' weblink="https://surajverma-07.github.io/gym-website/" text='My first project: a gym portfolio website crafted using HTML and CSS. Featuring a simple UI, it showcases gym services and includes a user-friendly form for entering details.' />
       </div>
     </div>
   );
